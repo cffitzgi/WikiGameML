e8beacd44a63286c64e35eae93a0667c09755542
brit1515 <55241306+brit1515@users.noreply.github.com>
2022-12-11 01:30:32 +0000

    added astar function from mp2 to demo.py

--- demo.py
+++ demo.py
@@ -20,4 +20,34 @@ def demo():
         #run A* search over current database
         #if not found, expand to n+1 from starting_article
     #print path
-    
+
+def astar(starting_node, goal_node):
+    # complete the function body: f = g + h
+    path = {}
+    path[starting_node.g] = starting_node
+    paths = {}
+    cost = 0
+    if starting_node == goal_node:
+        tempCost = starting_node.h + starting_node.g
+        return tempCost, path
+    children = starting_node.connected_to
+    for x in children:
+        tempCost, tempPath = astar(x, goal_node)
+        if tempPath is not None:
+            f = tempCost
+            f_limit = cost
+            if tempCost < f_limit:
+                f_limit = tempCost
+            path[x.g] = tempPath
+            paths[x.g] = [tempCost, path]
+    if len(paths) > 0:
+        cost = 1000000
+        for a in paths:
+            if paths[a][0] <= cost:
+                cost = paths[a][0]
+        for a in paths:
+            if paths[a][0] == cost:
+                return (a,paths[a][0])
+
+    print('No Path Found')
+    return None, None
