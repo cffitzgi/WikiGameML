4068f074d9529db32b1057661a3a574d6637336d
brit1515 <55241306+brit1515@users.noreply.github.com>
2022-12-17 01:16:23 +0000

    added method to assemble a graph using a root node

--- ArticleController.py
+++ ArticleController.py
@@ -10,5 +10,16 @@ def AssembleGraph(vertices: dict):
     print("Graph assembled.")
     return graph
 
+def AssembleRootedGraph(vertices: dict, root_node: ArticleModel.Vertex, graph: ArticleModel.Graph):
+    for child in root_node.connected_to:
+        if not child in graph.vertices_list:
+            root_node.connected_to[child] = vertices[child]
+    for child in (graph.vertices_list[root_node.id]).connected_to:
+        AssembleRootedGraph(vertices, child, graph)
+    return graph
+
+def ApplyHeuristic(heuristic):
+    print("Unfinished.")
+
 #class Controller:
 
